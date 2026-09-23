28083c06f347fdfd7d11b6c1a136976df01b259f
akshay1117 <akshaykrishnantv28@gmail.com>
2026-09-23 21:04:47 +0530

    refactor: remove unused imports and delete scratch web notifications file

--- lib/features/habits/presentation/screens/reminder_screen.dart
+++ lib/features/habits/presentation/screens/reminder_screen.dart
@@ -3,7 +3,6 @@ import 'package:kaizen/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter/foundation.dart';
 class ReminderScreen extends StatefulWidget {
   final String? initialReminder;
 
